51a542dcba1c4678a76ae9cd08efb16209c9a2f8
ericazaja <ericazaja@gmail.com>
2022-12-07 17:05:31 +0000

    E works on allometry script

--- scripts/allometry.R
+++ scripts/allometry.R
@@ -8,9 +8,9 @@ library(readxl)
 
 # 2. LOADING DATA ----
 
-# Biomass harvests and heights from Cunliffe et al 2020: QHI 
+# Biomass harvests (grams) and heights from Cunliffe et al 2020: QHI 
 Andy_biomass <- read_csv("data/allometry/Andy_paper/biomass.csv")
-Andy_heights <- read_csv("data/allometry/Andy_paper/heights.csv")
+Andy_heights <- read_excel("data/allometry/Andy_paper/heights.xlsx")
 
 # Biomass harvests and heights from Myers-Smith PhD: Pika  
 Biomass_harvest_Pika <- read_excel("data/allometry/Isla_phd/Biomass_harvest_Pika.xlsx")
@@ -20,6 +20,16 @@ Heights_Regression_Pika <- read_excel("data/allometry/Isla_phd/Heights_Regressio
 Logan_data_biomass <- read_csv("data/allometry/Berner/Logan-data-biomass.csv")
 
 # 3. DATA WRANGLING -----
+Andy_biomass <- Andy_biomass[-1,] # removing row with words
+
+Andy_heights_salix <- Andy_heights %>%
+  filter(Species %in% c("Salix arctica", "Salix pulchra", "Salix richardsonii" ))
+
+Andy_heights_salix$Height <-as.numeric(Andy_heights_salix$Height)
+
+Andy_heights_salix <- Andy_heights_salix %>% na.omit()
+
+# merge biomass and heights data  by plot N 
 
 # 4. MODELLING: regression
 # biomass ~ height  
--- scripts/carbon.R
+++ scripts/carbon.R
@@ -7,7 +7,18 @@
 # carbon ratios in the tundra trait database
 load("~/Desktop/Github_repos.tmp/NEW_MSC_ZAJA_2022/data/allometry/carbon/try_ttt.RData")
 
-# 1. DATA EXPLORING ----
+# 2. DATA EXPLORING ----
 str(try.ttt)
 unique(try.ttt$TraitShort)
 unique(try.ttt$AccSpeciesName)
+
+# 3. DATA WRANGLING
+try.ttt$AccSpeciesName <- as.factor(try.ttt$AccSpeciesName )
+
+# subsetting to salixes of interest
+CN_Salix <- try.ttt %>%
+  filter(AccSpeciesName %in% c("Salix arctica", "Salix pulchra")) %>%
+  filter(TraitShort == "C.N.ratio")
+# CN ratio: a ratio of 20 means that there are 20 g of carbon for each 1 g of nitrogen in that organic matter
+# do I keep other traits? LeafN, LeafC ?
+
