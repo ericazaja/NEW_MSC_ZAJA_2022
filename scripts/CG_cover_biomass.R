02f340c1ee76e73885e417d3b7e78c263563218f
@ericazaja <ericazaja@gmail.com>
2023-01-30 14:06:42 +0000

    Erica applied allom equations to CG shrubs

--- scripts/CG_cover_biomass.R
+++ scripts/CG_cover_biomass.R
@@ -44,9 +44,33 @@ all_CG_growth_cover <- all_CG_growth %>%
   mutate(cover_percent = cover *100) %>%
   filter(cover_percent <=100) # setting max to 100% cover 
 
+all_CG_growth_cover$Sample_age <- as.numeric(all_CG_growth_cover$Sample_age )
+
+# calculate biomass for each species based on allometric equations
+# Salix richardsonii: Biomass = -626.32 + 21.27*height + 40.34*cover +- 386
+all_CG_growth_cover_biomass_SALRIC <- all_CG_growth_cover %>%
+  filter(Species == "Salix richardsonii" & population == "Southern") %>% # only keeping southern shrubs
+  mutate(biomass_per_m2 = -626.32 + (21.27*Canopy_Height_cm) + (40.34*cover_percent), 
+         biomass_error_high = biomass_per_m2 + 386,
+         biomass_error_low = biomass_per_m2 - 386)
+
+# Salix pulchra: Biomass = 343.20 -30.75*height + 63.34*cover +- 479.3
+all_CG_growth_cover_biomass_SALPUL <- all_CG_growth_cover %>%
+  filter(Species == "Salix pulchra" & population == "Southern") %>% # only keeping southern shrubs
+  mutate(biomass_per_m2 = 343.20  + (-30.75*Canopy_Height_cm) + (63.34*cover_percent), 
+         biomass_error_high = biomass_per_m2 + 479.3,
+         biomass_error_low = biomass_per_m2 - 479.3)
+
+# Salix arctica: Biomass = -3.71 + 2.72*height + 14.24*cover +- 34.75 
+all_CG_growth_cover_biomass_SALARC <- all_CG_growth_cover %>%
+  filter(Species == "Salix arctica" & population == "Southern") %>% # only keeping southern shrubs
+  mutate(biomass_per_m2 = -3.71  + (2.72*Canopy_Height_cm) + (14.24*cover_percent), 
+         biomass_error_high = biomass_per_m2 + 34.75 ,
+         biomass_error_low = biomass_per_m2 - 34.75 )
+
 # 3. Data visualisation -----
 
-# Cover over time CG (2013-2022) ----
+# Cover over time CG (2013-2022) 
 (scatter_cover_CG <- ggplot(all_CG_growth_cover) +
    geom_smooth(aes(x = Sample_age, y =cover_percent, colour = population, fill = population, group = population, method = "glm")) +
    geom_point(aes(x = Sample_age, y= cover_percent, colour = population, group = population), size = 1.5, alpha = 0.5) +
@@ -67,6 +91,67 @@ all_CG_growth_cover <- all_CG_growth %>%
          axis.title = element_text(size = 18),
          axis.text.x = element_text(angle = 0, vjust = 0.5, size = 15, colour = "black"),
          axis.text.y = element_text(size = 15, colour = "black")))
+
+# predicted biomass: salix rich.
+(scatter_biomass_CG_SALRICH <- ggplot(all_CG_growth_cover_biomass_SALRIC) +
+    geom_point(aes(x = Sample_age, y= biomass_per_m2), size = 1.5, alpha = 0.5, colour ="dark green" ) +
+    geom_smooth(aes(x = Sample_age, y= biomass_per_m2), method = "glm", colour = "dark green", fill = "dark green")) +
+    ylab("Salix richardsonii biomass (g/m2)") +
+    xlab("\n Sample age (n years)") +
+    scale_colour_viridis_d(begin = 0.1, end = 0.8) +
+    scale_fill_viridis_d(begin = 0.1, end = 0.8) +
+    theme_bw() +
+    theme(panel.border = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          strip.text = element_text(size = 15, color = "black", face = "italic"),
+          legend.title = element_text(size=15), #change legend title font size
+          legend.text = element_text(size=12),
+          axis.line = element_line(colour = "black"),
+          axis.title = element_text(size = 18),
+          axis.text.x = element_text(angle = 0, vjust = 0.5, size = 15, colour = "black"),
+          axis.text.y = element_text(size = 15, colour = "black"))
+
+# predicted biomass: salix pulchra
+(scatter_biomass_CG_SALPUL <- ggplot(all_CG_growth_cover_biomass_SALPUL) +
+    geom_point(aes(x = Sample_age, y= biomass_per_m2), size = 1.5, alpha = 0.5, colour ="dark green" ) +
+    geom_smooth(aes(x = Sample_age, y= biomass_per_m2), method = "glm", colour = "dark green", fill = "dark green")) +
+  ylab("Salix pulchra biomass (g/m2)") +
+  xlab("\n Sample age (n years)") +
+  scale_colour_viridis_d(begin = 0.1, end = 0.8) +
+  scale_fill_viridis_d(begin = 0.1, end = 0.8) +
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.text = element_text(size = 15, color = "black", face = "italic"),
+        legend.title = element_text(size=15), #change legend title font size
+        legend.text = element_text(size=12),
+        axis.line = element_line(colour = "black"),
+        axis.title = element_text(size = 18),
+        axis.text.x = element_text(angle = 0, vjust = 0.5, size = 15, colour = "black"),
+        axis.text.y = element_text(size = 15, colour = "black"))
+
+# predicted biomass: salix arctica
+(scatter_biomass_CG_SALARC <- ggplot(all_CG_growth_cover_biomass_SALARC) +
+    geom_point(aes(x = Sample_age, y= biomass_per_m2), size = 1.5, alpha = 0.5, colour ="dark green" ) +
+    geom_smooth(aes(x = Sample_age, y= biomass_per_m2), method = "glm", colour = "dark green", fill = "dark green")) +
+  ylab("Salix arctica biomass (g/m2)") +
+  xlab("\n Sample age (n years)") +
+  scale_colour_viridis_d(begin = 0.1, end = 0.8) +
+  scale_fill_viridis_d(begin = 0.1, end = 0.8) +
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.text = element_text(size = 15, color = "black", face = "italic"),
+        legend.title = element_text(size=15), #change legend title font size
+        legend.text = element_text(size=12),
+        axis.line = element_line(colour = "black"),
+        axis.title = element_text(size = 18),
+        axis.text.x = element_text(angle = 0, vjust = 0.5, size = 15, colour = "black"),
+        axis.text.y = element_text(size = 15, colour = "black"))
+
 
 
 
