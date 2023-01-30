7352f3e39b92a071dbf97a902d7d1262032c5d29
@ericazaja <ericazaja@gmail.com>
2023-01-30 16:14:15 +0000

    Erica creates itex cover script

---
+++ scripts/ITEX_cover.R
@@ -0,0 +1,9 @@
+# ITEX shrub cover over time 
+#### Script by Erica Zaja, created 30/01/23
+### Last updated: 30/01/23
+
+
+# Workflow:
+
+# Loading data
+load("~/Desktop/Github_repos.tmp/NEW_MSC_ZAJA_2022/data/ITEX/ITEX_database.RData")
