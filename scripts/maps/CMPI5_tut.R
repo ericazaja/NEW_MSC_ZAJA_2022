ea6cf089d900ccf1e373a14b99afe579e3283c7a
@ericazaja <ericazaja@gmail.com>
2023-03-07 13:29:17 +0000

    e found new data

--- scripts/maps/CMPI5_tut.R
+++ scripts/maps/CMPI5_tut.R
@@ -11,30 +11,33 @@ library(tidyverse)
 library(lubridate)
 library(RNetCDF)
 
-# setting working directory 
+# setting working directory to my hard drive
 setwd("/Volumes/Kluane_22/CHELSA_CMIP5")
 
 # download data (from hard drive)
-
 nc = open.nc("tasmax_Amon_GFDL-ESM4_ssp126_r1i1p1f1_gr1_201501-210012.nc")
+nc_85 = open.nc("tasmax_Amon_GFDL-ESM4_ssp585_r1i1p1f1_gr1_201501-210012.nc")
+
 
 tasmax.dates = as.Date(var.get.nc(nc, "time"), origin="1850-01-01 00:00:00")
+tasmax.dates_2= as.Date(var.get.nc(nc_85, "time"), origin="1850-01-01 00:00:00")
 
-tasmax.scenes = sapply(1:length(tasmax.dates), function(z) {
-  grid = var.get.nc(nc, "tasmax", start=c(NA, NA, z), count=c(NA, NA, 1))
+tasmax.scenes = sapply(1:length(tasmax.dates_2), function(z) {
+  grid = var.get.nc(nc_85, "tasmax", start=c(NA, NA, z), count=c(NA, NA, 1))
   x = raster(grid, xmn=-90, xmx=90, ymn=0, ymx=360,
              crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
   x = rotate(flip(t(x), 2))
   x = (x - 273.15)
   return(x) })
 
-close.nc(nc)
+close.nc(nc_85)
 
-plot(tasmax.scenes[[1000]], main=tasmax.dates[[1000]], 
+plot(tasmax.scenes[[1000]], main=tasmax.dates_2[[1000]], 
      col=colorRampPalette(c('navy', 'lightgray', 'red'))(32))
 
-tasmax_df <- as.data.frame(tasmax.scenes)
+tasmax_df <- as.data.frame(tasmax_df) # doesnt work
 
+# Extract 
 library(sf)
 library(tsbox)
 
