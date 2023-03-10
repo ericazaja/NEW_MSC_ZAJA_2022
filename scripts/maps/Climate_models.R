b2c8aa6ad61b599ab6b147dc2c6aaec3ce951f74
@ericazaja <ericazaja@gmail.com>
2023-03-10 08:59:37 +0000

    E makes a quick scatter of biomass ~ temp

--- scripts/maps/Climate_models.R
+++ scripts/maps/Climate_models.R
@@ -1,12 +1,14 @@
 # CLIMATE MODELS ------
 
+# libraries ----
+library(bayesplot)
+library(brms)
+
+# data ------
 coord.chelsa.combo.b <- read.csv("data/coord.chelsa.combo.b")
 
-
-# model: 2100 only----
-# BASICALLY WHAT I HAVE HERE IS PREDICTED TEMPS in 2100 AND biomass in 2020, so
-# I cant relate them, I need to multiply by my rate of change before
-
+# model: 2020 only----
+# biomass in 2020 ~ temp in 2020 
 model_test <- brms::brm(biomass_per_m2 ~ mean_temp_2020_C, 
                         data = coord.chelsa.combo.c, family = gaussian(), chains = 3,
                         iter = 5000, warmup = 1000, 
@@ -15,4 +17,21 @@ model_test <- brms::brm(biomass_per_m2 ~ mean_temp_2020_C,
 summary(model_test) #significant ! 
 plot(model_test) # great
 pp_check(model_test, type = "dens_overlay", ndraws  = 100) 
-library(bayesplot)
+
+# Quick data viz
+mod <- (conditional_effects(model_test))
+mod_data <- mod[[1]]
+
+(biomass_temp_2020 <-ggplot(mod_data) +
+    geom_point(data = coord.chelsa.combo.c, aes(x =mean_temp_2020_C, y =biomass_per_m2 ),
+               alpha = 0.5)+
+    geom_line(aes(x = effect1__, y = estimate__),
+              linewidth = 1.5) +
+    geom_ribbon(aes(x = effect1__, ymin = lower__, ymax = upper__),
+                alpha = .1) +
+    ylab("Shrub biomass (g/m2) \n") +
+    xlab("\n Mean temperature in 2020 (C)" ) +
+   # ylim(0, 1500) +
+    scale_color_brewer(palette = "Greys")+
+    scale_fill_brewer(palette = "Greys")+
+    theme_shrub())
