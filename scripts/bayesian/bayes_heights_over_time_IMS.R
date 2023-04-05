976483d0fc7be06a88c462a36f95c47e396ebba8
@ericazaja <ericazaja@gmail.com>
2023-04-05 12:38:16 +0100

    E understanding QHI heights

--- scripts/bayesian/bayes_heights_over_time_IMS.R
+++ scripts/bayesian/bayes_heights_over_time_IMS.R
@@ -15,6 +15,40 @@ library(readxl)
 # LOAD DATA -----
 # data 1999-2019
 QHI_1999_2022 <- read_csv("data/ITEX/pointfr_1999_2022_clean.csv")
+# upload new data from QHI repo
+salpuls <- read_csv("data/ITEX/salpuls.csv")
+view(salpuls)
+
+salpuls <- subset(salpuls,!is.na(Height..cm.))
+
+meansp <- ddply(salpuls,.(YEAR), summarise,
+                mean.height = mean(Height..cm.),
+                sd = sd(Height..cm.))
+
+avg_salpuls <- ddply(salpuls,.(YEAR, SUBSITE, PLOT), summarise,
+                     Mean.C.H = mean(Height..cm.))
+
+Salix_canopy_m <- MCMCglmm(Height..cm. ~ I(YEAR - 1998), random = ~ YEAR + PLOT, 
+                           family = "gaussian", data = salpuls, pr=TRUE, nitt = 100000, burnin = 20000, prior = prior2)
+summary(Salix_canopy_m)
+
+# keep only 2019 and 2022 
+QHI_2019_2022 <- QHI_1999_2022 %>%
+  filter(YEAR %in% c(2019, 2022)) %>% select(SUBSITE, PLOT, YEAR, X, Y, SPP, STATUS, Height) %>% 
+  filter(SPP == "Salix pulchra" & STATUS == "LIVE")
+
+QHI_2019_2022_max <- QHI_2019_2022 %>% 
+  group_by(SUBSITE, PLOT, YEAR) %>%
+  summarise(HeightMax = max(Height)) 
+
+salpuls_max <-  salpuls %>% 
+  group_by(SUBSITE, PLOT, YEAR) %>%
+  summarise(HeightMax = max(Height..cm)) 
+
+gimpse(salpuls_max)
+
+new_bind <- rbind(salpuls_max,QHI_2019_2022_max )
+
 
 # Something is wrong here - check with Mariana
 test <- QHI_1999_2022 %>% 
@@ -40,22 +74,41 @@ QHI_2022_cm <- QHI_2022 %>%
                                Height_mm == "FALSE"~ Height)) %>% 
   # filter(Height_cm > 10) %>% # This will remove things like roots and basal stems
   select(-Height) %>%
-  na.omit(Height_cm)
+  na.omit(Height_cm) 
 
 # keeping only max values
 QHI_2022_max <- QHI_2022_cm %>%
   group_by(SUBSITE, PLOT, YEAR, SPP, X, Y) %>%
   summarise(max_heights_cm = max(Height_cm)) %>%
   distinct() %>% # keeping one unique value
-  na.omit(max_heights_cm)
+  na.omit(max_heights_cm) %>%
+  mutate(max_heights_integer = round(max_heights_cm))
 
 range(QHI_2022_max$max_heights_cm) # 0.2 39.3 cm 
+hist(QHI_2022_max$max_heights_cm, breaks = 30) # not normal at all
+
+QHI_2022_max$SubsitePlot <- with(QHI_2022_max, paste0(SUBSITE, PLOT))
+
+# calculating a plot mean per year
+QHI_2022_max_2 <- QHI_2022_max %>%
+  group_by(SubsitePlot, YEAR) %>%
+  summarise(plot_mean = mean(max_heights_cm))
+hist(QHI_2022_max_2$plot_mean, breaks=30)
+
+# calculating a plot max
+QHI_2022_max_3 <- QHI_2022_max %>%
+  group_by(SubsitePlot, YEAR) %>%
+  summarise(plot_max = max(max_heights_cm))
+hist(QHI_2022_max_3$plot_max, breaks = 30)
+
+#QHI_2022_max <- QHI_2022_max %>%
+ # mutate(log_max_heights = log(max_heights_cm))
 
 # quick plot
 # raw data 
-(pulchra_height_plot <- QHI_2022_max %>%
-    ggplot(aes(x = YEAR, y = max_heights_cm)) +
-    geom_point(data = QHI_2022_max) +
+(pulchra_height_plot <- new_bind %>%
+    ggplot(aes(x = YEAR, y = HeightMax)) +
+    geom_point(data = new_bind) +
     geom_smooth(method = "lm") +
     scale_fill_brewer(palette = "Set2") +
     scale_color_brewer(palette = "Dark2") +
@@ -67,7 +120,9 @@ range(QHI_2022_max$max_heights_cm) # 0.2 39.3 cm
 # Model QHI pulchra heights over time-----
 
 range(QHI_2022_max$max_heights_cm) #  0.0 39.3
-hist(QHI_2022_max$max_heights_cm)
+hist(QHI_2022_max$max_heights_cm, breaks = 30)
+hist(QHI_2022_max$log_max_heights, breaks = 30)
+hist(QHI_2022_max$max_heights_integer, breaks = 30)
 
 QHI_2022_max$height_scale <- center_scale(QHI_2022_max$max_heights_cm)
 QHI_2022_max$SubsitePlot <- with(QHI_2022_max, paste0(SUBSITE, PLOT))
@@ -81,6 +136,12 @@ QHI_height_time <- brms::brm(max_heights_cm ~ Year_index + (Year_index|SubsitePl
                                data = QHI_2022_max,  family = gaussian(), chains = 3,
                                iter = 5000, warmup = 1000, 
                                control = list(max_treedepth = 15, adapt_delta = 0.99))
+
+# trying model with not normal distribution
+QHI_height_time_2 <- brms::brm(max_heights_integer ~ Year_index + (Year_index|SubsitePlot),
+                             data = QHI_2022_max,  family = zero_inflated_poisson(), chains = 3,
+                             iter = 5000, warmup = 1000, 
+                             control = list(max_treedepth = 15, adapt_delta = 0.99))
 
 summary(QHI_height_time)
 pp_check(QHI_height_time, type = "dens_overlay", ndraws = 100) 
