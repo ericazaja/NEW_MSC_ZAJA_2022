836f91e5dbfa1b5330521218fd90c710766a6256
@ericazaja <ericazaja@gmail.com>
2023-05-15 16:56:06 +0100

    climate models tables

--- scripts/maps/Climate_models.R
+++ scripts/maps/Climate_models.R
@@ -30,7 +30,45 @@ pp_check(temp_time_future, type = "dens_overlay", ndraws  = 100)
 # Save an object to a file
 saveRDS(temp_time_future, file = "outputs/models/temp_time_future.rds")
 # Restore the object
-readRDS(file ="outputs/models/temp_time_future.rds")
+temp_time_future <- readRDS(file ="outputs/models/temp_time_future.rds")
+#without random effects 
+model_summ_no_rf <- function(x) {
+  sum = summary(x)
+  fixed = sum$fixed
+  sigma = sum$spec_pars
+  obs = sum$nobs
+  
+  fixed$effect <- "fixed"  # add ID column for type of effect (fixed, random, residual)
+  sigma$effect <- "residual"
+  fixed$nobs <- obs  # add column with number of observations
+  sigma$nobs <- obs
+  
+  modelTerms <- as.data.frame(bind_rows(fixed, sigma))  # merge it all together
+}
+
+temp_time_future_mod <- model_summ_no_rf(temp_time_future)
+
+rownames(temp_time_future_mod) <- c(" Intercept ", "Year (index)", " sigma ")
+temp_time_future_mod$Rhat <- as.character(formatC(temp_time_future_mod$Rhat, digits = 2, format = 'f'))
+
+temp_time_future_table <- temp_time_future_mod %>% 
+  kbl(caption="Table. Future temperature in study area (2020-2100). ", 
+      col.names = c("Estimate", "Error", "Lower 95% CI", "Upper 95% CI",
+                    "Rhat", "Bulk effective sample size", "Tail effective sample size",
+                    "Effect", "Sample size"), # give the column names you want making sure you have one name per column!
+      digits=2, align = "c") %>%  # specify number of significant digits, align numbers at the centre (can also align "l" left/ "r" right)
+  kable_classic(full_width=FALSE, html_font="Helvetica") # can change fonts
+
+# optional: making specific column text in italics
+column_spec(temp_time_future_table, 2, width = NULL, bold = FALSE) # 2 is my example column number 
+
+save_kable(temp_time_future_table,file = "outputs/tables/temp_time_future_table.pdf", # or .png, or .jpeg, save in your working directory
+           bs_theme = "simplex",
+           self_contained = TRUE,
+           extra_dependencies = NULL,
+           latex_header_includes = NULL,
+           keep_tex = FALSE,
+           density = 300)
 
 # model: 2020 only----
 # biomass in 2020 ~ temp in 2020 
@@ -45,7 +83,28 @@ pp_check(model_test, type = "dens_overlay", ndraws  = 100)
 # Save an object to a file
 saveRDS(model_test, file = "outputs/models/biomass_temp_2020.rds")
 # Restore the object
-readRDS(file ="outputs/models/biomass_temp_2020.rds")
+model_test <- readRDS(file ="outputs/models/biomass_temp_2020.rds")
+
+model_test_mod <- model_summ_no_rf(model_test)
+rownames(model_test_mod) <- c(" Intercept ", "Mean July Temp (°C)", " sigma ")
+model_test_mod$Rhat <- as.character(formatC(model_test_mod$Rhat, digits = 2, format = 'f'))
+
+model_test_mod_table <- model_test_mod %>% 
+  kbl(caption="Table. Temperature 2020 in study area vs biomass (g/m2) ", 
+      col.names = c("Estimate", "Error", "Lower 95% CI", "Upper 95% CI",
+                    "Rhat", "Bulk effective sample size", "Tail effective sample size",
+                    "Effect", "Sample size"), # give the column names you want making sure you have one name per column!
+      digits=2, align = "c") %>%  # specify number of significant digits, align numbers at the centre (can also align "l" left/ "r" right)
+  kable_classic(full_width=FALSE, html_font="Helvetica") # can change fonts
+
+
+save_kable(model_test_mod_table,file = "outputs/tables/model_test_mod_table.pdf", # or .png, or .jpeg, save in your working directory
+           bs_theme = "simplex",
+           self_contained = TRUE,
+           extra_dependencies = NULL,
+           latex_header_includes = NULL,
+           keep_tex = FALSE,
+           density = 300)
 
 # Quick data viz
 mod <- (conditional_effects(model_test))
